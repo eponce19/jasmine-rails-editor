147ac3e0d9bbffa5a7722a126f1530ff3b15d1f3
eponce19 <eponce19@gmail.com>
2016-09-13 14:17:24 -0500

    If I never see your face again

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,11 @@
+class WelcomeController < ApplicationController
+  def index
+    #code
+  end
+  def spec
+    #code
+  end
+  def sandbox
+    #code
+  end
+end
---
+++ config/initializers/assets.rb
@@ -0,0 +1,28 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( try-it.css )
+# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( ace/mode-coffee.js )
+Rails.application.config.assets.precompile += %w( underscore-min.js )
+Rails.application.config.assets.precompile += %w( coffee-script.js )
+Rails.application.config.assets.precompile += %w( ace/ace-uncompressed.js )
+Rails.application.config.assets.precompile += %w( ace/mode-coffee.js )
+Rails.application.config.assets.precompile += %w( ace/mode-javascript.js )
+Rails.application.config.assets.precompile += %w( js2coffee.min.js )
+Rails.application.config.assets.precompile += %w( backbone-min.js )
+Rails.application.config.assets.precompile += %w( jasmine-1.1.0/jasmine.js )
+Rails.application.config.assets.precompile += %w( jasmine-1.1.0/jasmine-html.js )
+Rails.application.config.assets.precompile += %w( jasmine-fixture-1.0.2.js )
+Rails.application.config.assets.precompile += %w( jasmine-stealth-0.0.9.js )
+Rails.application.config.assets.precompile += %w( jasmine-given-2.4.0.js )
+Rails.application.config.assets.precompile += %w( jasmine-jquery-1.3.1.js )
+Rails.application.config.assets.precompile += %w( spec-helper.js )
+Rails.application.config.assets.precompile += %w( try-it.js )
